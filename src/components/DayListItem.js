806354e72c610d3a848974d4b64bff874f113fc1
Rubin J <rubin@de-la-Fuego.local>
2020-07-06 19:49:18 -0700

    Add tests and write function to pass tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,6 +5,18 @@ import 'components/DayListItem.scss'
 const classNames = require('classnames');
 
 export default function DayListItem(props) {
+
+  const formatSpots = (spot) => {
+    if (!spot) {
+      return 'no spots remaining';
+    }
+    if (spot === 1) {
+      return '1 spot remaining';
+    } else {
+      return `${spot} spots remaining`;
+    }
+  };
+
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': !props.spots
@@ -16,7 +28,7 @@ export default function DayListItem(props) {
       onClick={() => props.setDay(props.name)}
     >
       <h2 className='text--regular'>{props.name}</h2>
-      <h3 className='text--light'>{props.spots}</h3>
+      <h3 className='text--light'>{formatSpots(props.spots)}</h3>
     </li>
   );
 }
