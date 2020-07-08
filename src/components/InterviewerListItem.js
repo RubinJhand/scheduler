d40b27fc9a4266f0aba1346f051aca4d94f2be35
Rubin J <rubin@praxis.local>
2020-07-07 20:35:16 -0700

    Refactor components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,16 +5,16 @@ import 'components/InterviewerListItem.scss';
 import classNames from 'classnames';
 
 export default function InterviewerListItem(props) {
-  const { name, avatar, selected } = props;
+	const { name, avatar, selected, setInterviewer } = props;
 
-  const interviewerClass = classNames('interviewers__item', {
-    'interviewers__item--selected': selected,
-    'interviewers__item-image': avatar,
-  });
-  return (
-    <li className={interviewerClass}>
-      <img className={interviewerClass} src={avatar} alt={name} />
-      {name}
-    </li>
-  );
+	const interviewerClass = classNames('interviewers__item', {
+		'interviewers__item--selected': selected,
+		'interviewers__item-image': avatar,
+	});
+	return (
+		<li className={interviewerClass} onClick={setInterviewer}>
+			<img className={interviewerClass} src={avatar} alt={name} />
+			{selected && name}
+		</li>
+	);
 }
