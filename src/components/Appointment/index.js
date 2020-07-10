0f5fdde3a7df178dc36da08468f511c34e459464
Rubin J <rubin@praxis.local>
2020-07-09 19:38:38 -0700

    Begin implementation of custom Hook in Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,17 +5,37 @@ import './styles.scss';
 import Show from './Show';
 import Empty from './Empty';
 import Header from './Header';
+import Form from 'components/Appointment/Form';
+
+import useVisualMode from 'hooks/useVisualMode';
 
 export default function Appointment(props) {
-	const { interview, time } = props;
-	return (
-		<article className='appointment'>
-			<Header time={time} />
-			{interview ? (
-				<Show student={interview.student} interviewer={interview.interviewer} />
-			) : (
-				<Empty />
-			)}
-		</article>
-	);
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE';
+  const { interview, time } = props;
+
+  //if (interview) then SHOW, if empty then EMPTY
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
+  return (
+    <article className='appointment'>
+      <Header time={time} />
+      {mode === EMPTY && (
+        //update onAdd to transition to CREATE mode
+        <Empty onAdd={() => transition(CREATE)} />
+      )}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />
+      )}
+      {/* when mode = CREATE show Form component */}
+      {mode === CREATE && (
+        //onCancel use back function to return to EMPTY state
+        <Form interviewers={[]} onCancel={() => back()} />
+      )}
+    </article>
+  );
 }
