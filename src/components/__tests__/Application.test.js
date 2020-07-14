529cdea926d73f5c82cfbb28d93c6ea182e10180
Rubin J <rubin@praxis.local>
2020-07-14 14:41:48 -0700

    Add tests for loading data, interview booking, reduce spots remaining

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -4,7 +4,12 @@ import {
   render,
   cleanup,
   waitForElement,
-  fireEvent
+  fireEvent,
+  getByText,
+  prettyDOM,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText
 } from '@testing-library/react';
 
 import Application from 'components/Application';
@@ -12,8 +17,6 @@ import Application from 'components/Application';
 afterEach(cleanup);
 
 describe('Application', () => {
-  // it("renders without crashing", () => {
-  // render(<Application />);
   it('defaults to Monday and changes the schedule when a new day is selected', () => {
     const { getByText } = render(<Application />);
 
@@ -22,4 +25,34 @@ describe('Application', () => {
       expect(getByText('Leopold Silvers')).toBeInTheDocument();
     });
   });
+
+  it('loads data, books an interview and reduces the spots remaining for Monday by 1', async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+
+    const appointments = getAllByTestId(container, 'appointment');
+    const appointment = appointments[0];
+
+    fireEvent.click(getByAltText(appointment, 'Add'));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: 'Lydia Miller-Jones' }
+    });
+    fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
+    fireEvent.click(getByText(appointment, 'Save'));
+
+    console.log(prettyDOM(appointment));
+  });
 });
+
+/* Alternative: use async () => {}
+it('changes the schedule when a new day is selected', async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText('Monday'));
+
+  fireEvent.click(getByText('Tuesday'));
+
+  expect(getByText('Leopold Silvers')).toBeInTheDocument();
+});
+*/
